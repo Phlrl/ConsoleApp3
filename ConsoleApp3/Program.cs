a474db8575c97536627cbca12c0e59d53afc9440
Phlrl <philipp129999@gmai.com>
2026-08-18 13:27:14 +0200

    First commit for game

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Game();
+        }
+
+        static void Game()
+        {
+            List<string> Player = new List<string> { "Player1", "Player2" };
+
+            int[] array1 = [5, 4, 3, 2, 1];
+
+            Console.WriteLine("Countdown:");
+
+            foreach (int value in array1)
+            {
+                Console.WriteLine(value);
+                Thread.Sleep(1000);
+            }
+
+            Console.WriteLine("bomb");
+
+            Bomb_Check(Player);
+
+            Console.WriteLine("Spieler:");
+
+            foreach (string player in Player)
+            {
+                Console.WriteLine(player);
+            }
+        }
+
+        static void Bomb_Check(List<string> Player)
+        {
+            string input = Console.ReadLine();
+
+            if (input == "bomb")
+            {
+                Player.Clear();
+            }
+        }
+    }
+}
