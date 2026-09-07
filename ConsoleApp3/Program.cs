232ed1317cfa18ba794ff2350ac7afec8575fc12
Phlrl <philipp129999@gmail.com>
2026-09-07 20:18:06 +0200

    optimization

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -19,7 +19,7 @@ namespace ConsoleApp3
 
         static void Game()
         {
-
+            
             Console.WriteLine("Player Names:");
             Console.WriteLine("");
 
@@ -30,7 +30,7 @@ namespace ConsoleApp3
 
             string[] players = new string[10];
 
-            for (int i = 0; i< 10; i++)
+            for (int i = 0; i< players.Length; i++)
             {
                 rInt = r.Next(0, Player.Count);
 
@@ -39,20 +39,12 @@ namespace ConsoleApp3
                 Player.RemoveAt(rInt);
             }
 
-            Random rng = new Random();
+            int zufallsZahl = r.Next(10000, 99999);
 
-            int zufallsZahl = rng.Next(10000, 99999);
-
-            Console.WriteLine("Player 1:    " + players[0]);
-            Console.WriteLine("Player 2:    " + players[1]);
-            Console.WriteLine("Player 3:    " + players[2]);
-            Console.WriteLine("Player 4:    " + players[3]);
-            Console.WriteLine("Player 5:    " + players[4]);
-            Console.WriteLine("Player 6:    " + players[5]);
-            Console.WriteLine("Player 7:    " + players[6]);
-            Console.WriteLine("Player 8:    " + players[7]);
-            Console.WriteLine("Player 9:    " + players[8]);
-            Console.WriteLine("Player 10:   " + players[9]);
+            for (int i = 0; i < players.Length; i++)
+            {
+                Console.WriteLine($"Player {i + 1}:   ".PadRight(13) + players[i]);
+            }
             Console.WriteLine("");
             Console.Write("Bomb Code:   ");
             Console.WriteLine(zufallsZahl);
@@ -153,8 +145,13 @@ namespace ConsoleApp3
             Console.WriteLine("|              GAME OVER             |");
             Console.WriteLine("+====================================+");
             Console.WriteLine();
-            //richtiger player als dead anzeigen 
-            PlayerDead(13, 2, "dead     ");
+            Console.Clear();
+
+            for (int i = 0; i < players.Length; i++)
+            {
+                Console.WriteLine($"Player {i + 1}:   ".PadRight(13) + players[i] + " ist tot");
+            }
+
             Thread.Sleep(30000);
 
 
@@ -176,11 +173,7 @@ namespace ConsoleApp3
             Console.WriteLine("+----------------------------------+");
         }
 
-         public static void PlayerDead(int column, int zeile, string neuerText)
-         {
-            Console.SetCursorPosition(column, zeile);
-            Console.WriteLine(neuerText);
-         }
+
     }
 
 }
